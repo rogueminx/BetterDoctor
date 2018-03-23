02794b984b814da2435e2ca3bd279cda2ee906a8
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-23 15:02:14 -0700

    can search successfully by symptom.

--- src/doctor-api.js
+++ src/doctor-api.js
@@ -23,7 +23,8 @@ export class Doctor {
   doctorBySymptom(symptom) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `...`;
+      let location = "or-portland";
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=${location}&skip=0&limit=10&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -32,4 +32,31 @@ $(document).ready(function() {
         $('#doctorNameError').text(`There was an error processing your request: ${error.message}`);
       };
   })
+
+  $("#doctorSymptom-search").submit(function(event) {
+    event.preventDefault();
+    let symptom = $("#symptom").val();
+    let secondSearch = new Doctor();
+    let secondpromise = secondSearch.doctorBySymptom(symptom);
+    secondpromise.then(function(response) {
+      if (response.length == 0) {
+        $('#symptomResults').append(`There are no doctors in your area that can help you with that particular problem.`)
+      } else {
+      let newresults = JSON.parse(response);
+        for(let i = 0; i < newresults.data.length; i++) {
+          let docFirstName = newresults.data[i].profile.first_name;
+          let docLastName = newresults.data[i].profile.last_name;
+          let streetAddress = newresults.data[i].practices[0].visit_address.street;
+          let docCity = newresults.data[i].practices[0].visit_address.city;
+          let zipcode = newresults.data[i].practices[0].visit_address.zip;
+          let phoneNumber = newresults.data[i].practices[0].phones[0].number;
+          let website = newresults.data[i].practices[0].website || "no website";
+          let newPatients = newresults.data[i].practices[0].accepts_new_patients;
+          $('#symptomResults').append(`<h5>Doctor Name: ${docLastName}, ${docFirstName}</h5>Address: <br>${streetAddress}, ${docCity}, ${zipcode}<br> Phone Number: <br>${phoneNumber} <br> Website: <br>${website} <br> Accepting New Patients: <br>${newPatients} `);
+        }
+      }
+      }),function(statusText) {
+        $('#symptomError').text(`There was an error processing your request: ${error.message}`);
+      };
+  })
 });
